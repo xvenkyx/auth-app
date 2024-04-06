7a72b3a410de1555945ec65a2ac6e6d993c25cc6
xvenkyx <venkyroxx99@gmail.com>
2024-04-06 10:45:46 +0530

    mongodb connected

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import dotenv from "dotenv"
+import connect2mongo from "./db/connect2mongo.js";
+dotenv.config();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
+  connect2mongo()
   console.log(`Server running on port - ${port}`);
 });
